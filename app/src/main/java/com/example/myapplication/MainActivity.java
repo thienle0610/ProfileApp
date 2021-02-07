c248140d5aa43d7e5fe53753845a15930f425e98
thienle0610 <67705332+thienle0610@users.noreply.github.com>
2021-02-06 18:06:38 -0800

    Finished Thien's Profile

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText profileDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendProfile(View view){
+        Intent intent = new Intent(this, DisplayThienProfile.class);
+        startActivity(intent);
     }
 }
