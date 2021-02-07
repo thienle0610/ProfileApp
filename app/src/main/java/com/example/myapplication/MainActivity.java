9b1eb5296596602a272d08d2c25ff15e175772bd
Uyen[Brianna] Pham <brianna.pham799@gmail.com>
2021-02-06 19:26:04 -0800

    Uyen has changed it

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendProfile(View view){
+        public void sendProfile (View view){
         Intent intent = new Intent(this, DisplayThienProfile.class);
         startActivity(intent);
     }
+
+        public void sendUyenProfile(View v){
+            Intent in = new Intent(this, UyenProfile.class);
+            startActivity(in);
+        }
+
 }
